12748cdfba5ada31414032a8e359eee72e7a173d
wwsheng009 <vincentwwsheng@gmail.com>
2025-06-04 13:16:16 +0800

    refactor(init.sh): 改进yao命令路径的检测逻辑
    添加对yao命令路径的动态检测，优先检查/usr/local/bin/yao，其次检查/usr/bin/yao，最后使用which命令查找。这使得脚本在不同环境中更具可移植性。

--- init.sh
+++ init.sh
@@ -3,16 +3,22 @@
 ENV_FILE=".env"
 mkdir db
 
+YAO_PATH="/usr/bin/yao"
+if [ -f "/usr/local/bin/yao" ]; then
+    YAO_PATH="/usr/local/bin/yao"
+else
+    YAO_PATH=$(which yao)
+fi
 # Check if the .env file exists
 if [ -f "$ENV_FILE" ]; then
     echo ".env file found. Running 'yao migrate'..."
     # Run yao migrate
-    /usr/local/bin/yao migrate
+    $YAO_PATH migrate
 else
     echo ".env file not found. Copying from .env.sample..."
     cp .env.sample .env
-    /usr/local/bin/yao migrate
+    $YAO_PATH migrate
 fi
 mv data/templates/default/template.json.bak data/templates/default/template.json
-/usr/local/bin/yao sui build web default
-/usr/local/bin/yao start
+$YAO_PATH sui build web default
+$YAO_PATH start
