052f9e83f2fbfdf9904b11f7221aaeea578b7dc2
wwsheng009 <vincentwwsheng@gmail.com>
2025-06-07 15:40:58 +0800

    build(docker): update code and rebuild docker image
    - Pull latest code changes
    - Rebuild Docker image with tag yao-cui-demo
    - Run container with restart policy and port mapping

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
 docker rm -f yao-cui-demo
+git pull
 docker build --build-arg ARCH=amd64 --build-arg VERSION=0.10.5 --tag yao-cui-demo .
 docker run -d --restart unless-stopped --name yao-cui-demo -p 7099:5099 yao-cui-demo
