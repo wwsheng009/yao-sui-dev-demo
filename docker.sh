0e2a7eedbf83b087ff10bac7d12e6a89d6f9facc
wwsheng009 <vincentwwsheng@gmail.com>
2025-06-07 10:39:41 +0800

    "feat(国际化): 添加多语言支持并修复Docker命令错误"

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
-docker rf -f yao-cui-demo
+docker rm -f yao-cui-demo
 docker build --build-arg ARCH=amd64 --build-arg VERSION=0.10.5 --tag yao-cui-demo .
 docker run -d --restart unless-stopped --name yao-cui-demo -p 7099:5099 yao-cui-demo
---
+++ scripts/lang.ts
@@ -0,0 +1,6 @@
+//yao run scripts.lang.trans
+function trans(){
+    console.log($L("Encode"));
+    console.log($L("Encode","zh-cn"));
+    console.log($L("Encode","zh-hk"));
+}
